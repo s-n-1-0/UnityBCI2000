2312f926db132be8aa19095f9703e13faef26f58
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-08-12 00:14:45 +0900

    remove docs

--- docs/html/search/all_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['initcommands_0',['initCommands',['../classUnityBCI2000.html#af6e0150f6d172bb5166bc8580b6b294c',1,'UnityBCI2000']]]
-];
--- docs/html/search/all_8.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['unitybci2000_0',['UnityBCI2000',['../md_README.html',1,'(Global Namespace)'],['../classUnityBCI2000.html',1,'UnityBCI2000']]]
-];
